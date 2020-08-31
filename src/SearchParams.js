bf558e6b01772e91499d0b9044cdd4bd44364adb
Aaryan Dewan <aaryandewan@yahoo.com>
2020-08-31 13:29:36 +0530

    initial commit

---
+++ src/SearchParams.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  //const location = "Seattle WA"; -> this is hardcoded
+
+  const [location, setLocation] = useState("Seattle WAA");
+  const [animal, setAnimal] = useState("Dog");
+  const [breed, setBreed] = useState("German Shepherd");
+  const [breeds, setBreeds] = useState([]);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value="animal"
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option>ALl</option>
+            {breeds.map((breedstring) => (
+              <option key={breedstring} value={breedstring}>
+                {breedstring}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
---
+++ src/useDropdown.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const useDropdown = (label, defaultState, options) => {
+  cosnt[(state, setstate)] = useState(defaultState);
+  const id = `use-dropdown${label.replace(" ", "").toLowerCase()}`;
+};
+
+export default useDropdown;
